1f517079b247488d7b60a48c72c1528025650094
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2022-09-14 13:07:06 +0530

    basic site done :-mrigesh

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,28 +4,32 @@ import "./features.css";
 
 const featuresData = [
   {
-    title: "Improving end distrusts instantly",
-    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+    title: "Pandemic/Natural Disasters",
+    text: "Integrate an approach that helps people build up resilience to cope with the inevitable new normal by figuring strategies in the wake of pandemic.",
   },
   {
-    title: "Become the tended active",
+    title: "Block-Chain",
     text: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.",
   },
   {
-    title: "Message or am nothing",
-    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.",
+    title: "Cyber Bullying",
+    text: "Build prudent solutions to help prevent people from succumbing to the spiral of cyberbullying and to combat misuse of technology and online harassment.",
   },
   {
-    title: "Really boy law county",
-    text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..",
+    title: "Open Innovation",
+    text: "Let your imagination go haywire as you envision ideas that hold the capability to solve any problem that has been distressing mankind for ages.",
   },
 ];
 
 const Features = () => (
   <div className="gpt3__features section__padding" id="features">
     <div className="gpt3__features-heading">
-      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
-      <p>Request Early Access to Get Started</p>
+      <h1 className="gradient__text">
+        The Future is Now and You Just Need to Realize It.{" "}
+      </h1>
+
+      <h1 className="gradient__text">Explore the problem statements .</h1>
+      <p>Register Now and Get Started</p>
     </div>
     <div className="gpt3__features-container">
       {featuresData.map((item, index) => (
